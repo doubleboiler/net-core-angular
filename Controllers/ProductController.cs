89ce67ecbba941d7f29e27791f709e454f7fce62
Константин Бирюков <birconjam@gmail.com>
2019-07-04 22:43:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ANGULAR.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ANGULAR.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        ApplicationContext db;
+        public ProductController(ApplicationContext context)
+        {
+            db = context;
+            if (!db.Products.Any())
+            {
+                db.Products.Add(new Product { Name = "iPhone X", Company = "Apple", Price = 79900 });
+                db.Products.Add(new Product { Name = "Galaxy S8", Company = "Samsung", Price = 49900 });
+                db.Products.Add(new Product { Name = "Pixel 2", Company = "Google", Price = 52900 });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            return product;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANGULAR.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+    }
+}
