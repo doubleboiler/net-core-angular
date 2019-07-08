c90853479f3dc8995a748924a456460d67b052d2
Константин Бирюков <birconjam@gmail.com>
2019-07-08 13:13:35 +0300

    add product list

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,11 +2,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+
+// определение маршрутов
+const appRoutes: Routes = [
+    { path: '', component: ProductListComponent },
+    { path: 'product/:id', component: ProductDetailComponent },
+    { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+    declarations: [AppComponent, ProductListComponent, ProductDetailComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
